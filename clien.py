fb18deb5656412f8cbe2d2af9bba3113ffdea789
Zhabelle <dmunoz024@gmail.com>
2022-05-16 17:58:50 -0500

    código para proyecto

--- clien.py
+++ clien.py
@@ -1,12 +1,22 @@
+import os
 import socket
+import tkinter as tk
+from tkinter import filedialog
 
 
 if __name__ == "__main__":
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(("localhost", 65432))
-    f = open("datos.txt","rb")
-    s.send(f.name.encode())
-    s.recv(1024)
-    a = f.read()
-    s.sendall(a)
+    loginData = (input("Introduzca su nombre de usuario: "), input("Introduzca su contraseña: "))
+    s.sendall(str(loginData).encode())
+    if len(s.recv(1024).decode())<1:
+        raise Exception("Error de autenticación")
+    root = tk.Tk()
+    root.withdraw()
+    root.wm_attributes('-topmost', 1)
+    file_path = filedialog.askopenfilename(initialdir=os.getcwd(), filetypes=[("Text document", ".txt")], parent=root)
+    if(file_path == None):
+        raise Exception("Archivo no especificado")
+    f = open(file_path,"rb")
+    s.sendall(f.read())
     s.close()
--- serv.py
+++ serv.py
@@ -1,33 +1,74 @@
 import nacl.utils as pynacl
 import nacl.secret as pysecret
-import socket
+import socket, time
 from Crypto.Cipher import ChaCha20
-from nacl.signing import SigningKey
+from nacl.signing import SigningKey, VerifyKey
 
-def getRand() -> str:
-    bs = pynacl.random(size=128)
-    r = 0
-    for c in bs:
-        r += c
-    return '{0:X}'.format(r)
+
+userData = {
+    "taqueches": "barbacha",
+    "fido": "el_perro",
+    "a": "b"
+}
 
 if __name__ == "__main__":
-    key = pynacl.random(pysecret.SecretBox.KEY_SIZE)
-    cipher = ChaCha20.new(key=key)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(("localhost", 65432))
     s.listen()
     c,a = s.accept()
     print(f"{a} conectado")
+
+#   Nombre de usuario y contraseña
+    print("\n-Nombre de usuario y contraseña")
+    loginData = c.recv(1024).decode().strip("()").replace("'","").split(", ")
+    loginError = loginData[0] not in userData.keys() or userData[loginData[0]] != loginData[1]
+    if not loginError:
+        c.sendall("autenticación existosa".encode())
     
-    filename = c.recv(1024).decode()
-    filename = filename.replace(".txt", "_serv.txt")
-    c.send("a".encode())
+#   Bitácora de accesos (logs)
+    print("\n-logs")
+    mensaje = f"[{time.strftime('%d.%m.%G-%H:%M:%S')}] [{'WARN' if loginError else 'INFO'}] \"{loginData[0]}\" ha {'intentado iniciar' if loginError else 'iniciado'} sesión.\n"
+    f = open("bitácora.txt", "a", encoding='utf-8')
+    f.write(mensaje)
+    print(mensaje)
+    f.close()
+
+    if loginError:
+        c.close()
+        s.close()
+        raise Exception("Error de autenticación")
+    
+#   Generación y Recuperación de Claves hacia o desde 1 archivo
+    print("\n-Generación y Recuperación de Claves hacia o desde 1 archivo")
+    
+#   Cifrado de Archivos
+    print("-Cifrado de archivo")
+    filename = "serv_file.txt"
     f = open(filename, "wb")
     data = c.recv(1024)
+    if len(data.decode()) < 1:
+        raise Exception("No se recibieron datos")
+    
+    key = pynacl.random(pysecret.SecretBox.KEY_SIZE)
+    cipher = ChaCha20.new(key=key)
     encryptedData = cipher.encrypt(data)
-    signedData = SigningKey.generate().sign(encryptedData)
+    nonce = cipher.nonce
+    print(encryptedData)
+#   Descifrado de Archivos
+    print("\n-Decifrado de archivo")
+    cipher = ChaCha20.new(key=key, nonce=nonce)
+    decryptedData = cipher.decrypt(encryptedData)
+    print(decryptedData)
+#   Firma de Archivos
+    print("\n-Firma de archivo")
+    sign_key = SigningKey.generate()
+    signedData = sign_key.sign(encryptedData)
+    print(signedData)
     f.write(signedData)
+#   Verificación de Firma de Archivos
+    print("\n-Verificación de firma")
+    verify_key = VerifyKey(sign_key.verify_key.encode())
+    print(verify_key.verify(signedData))
     
     c.close()
     s.close()
